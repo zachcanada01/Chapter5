174cb5608310155cc78e2af1bd92e78cc81f49cc
Zach <zachcanada23@gmail.com>
2018-09-27 09:46:06 -0500

    rock paper scissors updated

--- Chapter 5/src/gameZone/RockPaperScissors.java
+++ Chapter 5/src/gameZone/RockPaperScissors.java
@@ -1,10 +1,34 @@
 package gameZone;
 
+import java.util.Scanner;
+
 public class RockPaperScissors {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int computerGuess;
+		int userChoice;
+		String result = null;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("1-Rock\n2-Paper\n3-Scissors");
+		userChoice = input.nextInt();
+		
+		if(computerGuess == 1)
+		{
+			if(userChoice == 1)
+			{
+				result = "tie";
+			}
+		}
+		
+		displayMessage(result);
+			
 	}
-
+	
+	public static void displayMessage(String result)
+	{
+		System.out.println(result);
+	}
 }
